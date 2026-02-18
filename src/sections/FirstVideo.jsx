b8417ea74a464f4540b73a629e0761f7c527b5c6
creedU <ugond.7@gmail.com>
2026-02-18 18:46:07 +0530

    Changes in First Video Section

--- src/sections/FirstVideo.jsx
+++ src/sections/FirstVideo.jsx
@@ -1,46 +1,64 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import React, { useRef } from 'react'
+
+gsap.registerPlugin(ScrollTrigger);
+
 const FirstVideo = () => {
-    const videoRef = useRef(null);
-    
-        useGSAP(() => {
-            gsap.set(".first-vd", { marginTop: "-150vh", opacity: 0 });
+  const videoRef = useRef(null);
 
-            const tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: ".first-vd-wrapper",
-                    start: "top top",
-                    end: "+=200% top",
-                    scrub: true,
-                    pin: true,
-                }
-            })
-        tl.to(".hero-section", { opacity: 0, delay: 0.5 , ease: "power1.inOut"});    
-        tl.to(".first-vd-wrapper", { opacity: 1, duration:2, ease: "power1.inOut" });
+  useGSAP(() => {
 
-        videoRef.current.onloadedmetadata = () => {
-            tl.to(videoRef.current, {currentTime: videoRef.current.duration, duration: 3,ease: "power1.inOut"}, "<");
-        }
+    gsap.set(".first-vd-wrapper", { marginTop: "-150vh", opacity: 0 });
 
-        }, []);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".first-vd-wrapper",
+        start: "top top",
+        end: "+=200%",
+        scrub: true,
+        pin: true,
+      }
+    });
 
+    tl.to(".hero-section", {
+      opacity: 0,
+      ease: "power1.inOut"
+    });
+
+    tl.to(".first-vd-wrapper", {
+      opacity: 1,
+      duration: 2,
+      ease: "power1.inOut"
+    });
+
+    if (videoRef.current) {
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current.duration,
+          duration: 3,
+          ease: "power1.inOut"
+        }, "<");
+      };
+    }
+
+  }, []);
 
   return (
     <section className='first-vd-wrapper'>
-        <div className='h-dvh'>
-            <video 
-                ref={videoRef}
-                muted
-                playsInline
-                preload='auto'
-                src='/videos/output1.mp4'
-                className='first-vd'
-            />
-        </div>
-      
+      <div className='h-dvh'>
+        <video 
+          ref={videoRef}
+          muted
+          playsInline
+          preload='auto'
+          src='/videos/output1.mp4'
+          className='first-vd'
+        />
+      </div>
     </section>
   )
 }
 
-export default FirstVideo
+export default FirstVideo;
