3ba2a772935c92b4a2e9ae0f548837dc71713d38
creedU <ugond.7@email.com>
2026-02-24 18:50:40 +0530

    Added PostCard Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
+import PostCard from "./sections/PostCard";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -22,6 +23,7 @@ const app = () => {
       <SecondVideo />
       <Lucia  />
       
+      <PostCard />
     </main>
   )
 }
